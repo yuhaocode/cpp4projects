a756ef3156cd5499e7cd88429c0252eacd027866
Prof Ninja Novocin <andy@novocin.com>
2015-09-06 21:25:44 +0000

    created multi-module example

---
+++ avg.cpp
@@ -0,0 +1,7 @@
+// Note that we include the header for this file,
+// essentially allowing this file to fill that contract
+#include "avg.h"
+
+double average(double a, double b){
+    return (a + b)/2;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "avg.h"
+using namespace std;
+
+//this is the actual program and it gets to use our "module" just like any other.
+
+int main(){
+    cout << average(34.0, 36.0) << endl;
+    return 0;
+}
