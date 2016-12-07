0c8f871ae27cd2e2995b17bafe06976638ab4bf3
yuhaocode <pyh@udel.edu>
2016-12-07 14:45:48 -0500

    Delete avg.h

--- avg.h
+++
@@ -1,8 +0,0 @@
-/*
- *
- * This is the header file, avg.h, it declares the contract for our function.
- * Please note that the the variable names are optional here.
- * The declaration of the function, average, is in the file avg.cpp
- * 
- */
-double average(double a, double b);
