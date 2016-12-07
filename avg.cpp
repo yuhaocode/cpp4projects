2a73533b8166ddfa7ccb3880c6dc0255daee4cf2
yuhaocode <pyh@udel.edu>
2016-12-07 14:45:42 -0500

    Delete avg.cpp

--- avg.cpp
+++
@@ -1,7 +0,0 @@
-// Note that we include the header for this file,
-// essentially allowing this file to fill that contract
-#include "avg.h"
-
-double average(double a, double b){
-    return (a + b)/2;
-}
